7b55d190fb9c403d55b5dc56f22f729ada9ce314
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2015-03-05 20:08:16 +0100

    update version add model provided_dates

--- web/models/contact.ex
+++ web/models/contact.ex
@@ -11,6 +11,7 @@ defmodule Contact do
     field :country, :string
     belongs_to :user, User 
     has_many :types, Type
+    has_many :provided_dates, ProvidedDate
     field :created_at, :datetime, default: Ecto.DateTime.local
     field :updated_at, :datetime, default: Ecto.DateTime.local
     field :region, :string
---
+++ web/models/provided_date.ex
@@ -0,0 +1,13 @@
+defmodule ProvidedDate do
+  use Ecto.Model
+
+  schema "provided_dates" do
+    field :date_at, :datetime
+    field :date_to, :datetime
+    field :date_to_activation, :boolean
+    belongs_to :contact, Contact
+    belongs_to :item, Item
+    field :created_at, :datetime, default: Ecto.DateTime.local
+    field :updated_at, :datetime, default: Ecto.DateTime.local
+  end
+end
