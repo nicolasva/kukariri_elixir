cb4c361b75f6ad8dc67fbd2bed844f79778d19d8
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2015-03-07 22:48:45 +0100

    update version kukariri fix bug

--- web/models/login.ex
+++ web/models/login.ex
@@ -1,11 +1,11 @@
 defmodule Kukariri.Login do
-  use Ecto.Query
+  import Ecto.Query
   alias Kukariri.User
 
   def login?(email, password) do 
     query = from user in Users,
-      where user.email == ^email,
-      where user.password == ^password,
+      where: user.email == ^email,
+      where: user.password == ^password,
       select: user
     Repo.all(query) |> List.first
   end
