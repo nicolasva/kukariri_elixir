e403530b2624d530a3142a4621da57f65310be83
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2015-03-29 17:09:59 +0200

    refactoring code uploading picture and .fix button valid

--- web/controllers/pictures_controller.ex
+++ web/controllers/pictures_controller.ex
@@ -18,17 +18,10 @@ defmodule Kukariri.PicturesController do
   end
 
   def create(conn, params) do
-    item_id = String.to_integer(params["item_id"])
+    item_id = String.to_integer(params["item_id"]) 
     Enum.each(params["files"], fn(image) ->  
-      picture_upload = UploadFile.upload_img(image)
-      
-      picture = %Kukariri.Picture{item_id: item_id, picture_content_type: picture_upload.image_content_type, picture_file_name: picture_upload.image_filename, type: [descriptif: params["picture"]["type"]["descriptif"]]}
-      type_description = elem(Enum.at(picture.type, 0),1)
-      type = %Kukariri.Type{descriptif: type_description, item_id: item_id}
-      type = Kukariri.Repo.insert(type) 
-      picture = %Kukariri.Picture{position: 0, type_id: type.id, item_id: item_id, picture_content_type: picture_upload.image_content_type, picture_file_name: picture_upload.image_filename, type: [descriptif: params["picture"]["type"]["descriptif"]]}
-      picture = Kukariri.Repo.insert(picture)
-      json conn, %{files: [%{thumbnailUrl: "/images/img_items/#{picture.picture_file_name}", type: picture.picture_content_type, size: 174035, deleteUrl: "/images/img_items/#{picture.picture_file_name}", item_id: item_id, type_id: type.id, deleteType: "DELETE",  url: "/images/img_items/#{picture.picture_file_name}", picture_file_name: picture.picture_file_name}]}
+      picture = Kukariri.Picture.picture_save(image, item_id, params)
+      json conn, %{files: [%{thumbnailUrl: "/images/img_items/#{picture.picture_file_name}", type: picture.picture_content_type, size: 174035, deleteUrl: "/images/img_items/#{picture.picture_file_name}", item_id: item_id, type_id: picture.type_id, deleteType: "DELETE",  url: "/images/img_items/#{picture.picture_file_name}", picture_file_name: picture.picture_file_name}]}
     end
     )
   end
--- web/models/picture.ex
+++ web/models/picture.ex
@@ -10,4 +10,14 @@ defmodule Kukariri.Picture do
     belongs_to :item, Kukariri.Item
     belongs_to :type, Kukariri.Type
   end
+
+  def picture_save(image, item_id, params) do 
+      picture_upload = UploadFile.upload_img(image) 
+      picture = %Kukariri.Picture{item_id: item_id, picture_content_type: picture_upload.image_content_type, picture_file_name: picture_upload.image_filename, type: [descriptif: params["picture"]["type"]["descriptif"]]}
+      type_description = elem(Enum.at(picture.type, 0),1)
+      type = %Kukariri.Type{descriptif: type_description, item_id: item_id}
+      type = Kukariri.Repo.insert(type) 
+      picture = %Kukariri.Picture{position: 0, type_id: type.id, item_id: item_id, picture_content_type: picture_upload.image_content_type, picture_file_name: picture_upload.image_filename, type: [descriptif: params["picture"]["type"]["descriptif"]]}
+      Kukariri.Repo.insert(picture)
+  end
 end
